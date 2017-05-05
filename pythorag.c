c13c6285438d55ac2e090831ec3cd024ca18edfb
happysmash27 <happysmash27@vmail.me>
2017-05-04 22:20:35 -0700

    Added some error correction

--- pythorag.c
+++ pythorag.c
@@ -3,5 +3,9 @@
 #include <math.h>
 
 int main(int argc, char **argv){
+  if (argc!=3){
+    printf("Usage: ./pythorag <val1> <val2>\n");
+    return 1;
+  }
   printf("%f\n",sqrt((strtol(argv[1],NULL,10)*strtol(argv[1],NULL,10))+(strtol(argv[2],NULL,10)*strtol(argv[2],NULL,10))));
 }
